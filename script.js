9f9e192e35c8af2e589cff8d352fe68436cffc7d
Jaren Mendelsohn <jaren.mendelsohn@gmail.com>
2017-10-31 17:17:33 -0700

    Rhyme list generation

--- script.js
+++ script.js
@@ -9,17 +9,42 @@ function getStresses(word) {
     return word.trim().split(" ").map(x => window.wordData[x].pronounce.replace(/[^0-9]/g, "")).reduce((x, y) => x + y).split("").map(x => parseInt(x));
 }
 
-function checkIambic(word) {
+function checkIambic(word, reversed) {
+    // Only works with even syllables
     var stresses = getStresses(word).map(x => x == 0 ? 0 : 1);
     for (let i = 0; i < stresses.length; i++) {
-        if (stresses[i] != i % 2) return false; 
+        if (stresses[i] != (reversed ? i + 1 : i) % 2) return false;
     }
     return true;
 }
 
+function getRhymeStem(word) {
+    var pronounce = window.wordData[word.trim().split(" ").slice(-1)[0]].pronounce.replace(/[0-9]/g, "").split(" ").filter(x => x.length > 0);
+    pronounce.splice(0, pronounce.lastIndexOf(pronounce.filter(x => /[AEIOU]/.test(x)).slice(-1)[0]));
+    return pronounce.join(" ");
+}
+
 function getRhymes(word) {
-    word = word.trim().split(" ").slice(-1)[0];
-    // TODO
+    var lists = window.rhymeLists.filter(x => x.includes(word));
+    var list = [];
+    if (lists.length > 0) {
+        list = lists[0];
+        list.splice(list.indexOf(word), 1);
+    }
+    return list;
+}
+
+function forceGenerateLine(constraints, hideAlert) {
+    var line = null;
+    var tries = 0;
+    while (line == null) {
+        line = generateLine(constraints);
+        if (tries++ > window.maxTries) {
+            if (!hideAlert) alert("Poem generation failed, try again? :(");
+            return null;
+        }
+    }
+    return line;
 }
 
 function generateLine(constraints) {
@@ -42,12 +67,7 @@ function generateLine(constraints) {
     var word = "\n";
     if (constraints.syllables != null) {
         if (!constraints.notRoot) {
-            var result;
-            var tries = 0;
-            while (result == null) {
-                result = generateLine(Object.assign({}, constraints, { notRoot: true }));
-                if (tries++ > window.maxTries) return null;
-            }
+            var result = forceGenerateLine(Object.assign({}, constraints, { notRoot: true }));
             output = result.line;
             word = result.lastWord;
         } else {
@@ -77,15 +97,23 @@ function generateLine(constraints) {
 
 var poemFunctions = {
     freeform: function () {
-        return Array.apply(null, Array(10)).map(_ => generateLine({ }).line);
+        return Array.apply(null, Array(10)).map(_ => generateLine({}).line);
     },
 
     haiku: function () {
-        return [ generateLine({ syllables: 5 }).line, generateLine({ syllables: 7 }).line, generateLine({ syllables: 5 }).line ];
+        return [generateLine({ syllables: 5 }).line, generateLine({ syllables: 7 }).line, generateLine({ syllables: 5 }).line];
     },
 
     iambicPentameter: function () {
         return Array.apply(null, Array(10)).map(_ => generateLine({ syllables: 10, requireIambic: true }).line);
+    },
+
+    sonnet: function () {
+        function generateRhyme() {
+        }
+
+        var lines = Array.apply(null, Array(5)).map(_ => generateRhyme());
+        return [lines[0][0], lines[1][0], lines[0][1], lines[1][1], "", lines[2][0], lines[3][0], lines[2][1], lines[3][1], "", lines[4][0], lines[4][1]];
     }
 };
 
@@ -100,18 +128,18 @@ function init(url) {
     }
     sel.getElementsByTagName("option")[0].selected = "selected";
     fetch(url)
-    .then(data => data.json())
-    .then(json => {
-        window.possibleChains = json.chains;
-        window.wordData = json.wordData;
-        window.ready = true;
-        document.getElementById("generate").innerHTML = "Generate";
-    })
-    .catch(_ => {
-        alert("Invalid data!");
-        document.getElementById("generate").innerHTML = "Generate";
-        window.ready = true;
-    });
+        .then(data => data.json())
+        .then(json => {
+            window.possibleChains = json.chains;
+            window.wordData = json.wordData;
+            window.ready = true;
+            document.getElementById("generate").innerHTML = "Generate";
+        })
+        .catch(_ => {
+            alert("Invalid data!");
+            document.getElementById("generate").innerHTML = "Generate";
+            window.ready = true;
+        });
 }
 
 function generatePoem() {
@@ -120,6 +148,19 @@ function generatePoem() {
         return;
     }
     window.chain = window.possibleChains[document.getElementById("markovLength").selectedIndex];
+
+    // Build reverse chain
+
+    // Build rhyme list
+    window.rhymes = {};
+    var keys = Object.keys(window.chain).filter(x => x != "\n");
+    var stems = keys.map(x => getRhymeStem(x)); 
+    for (let i = 0; i < keys.length; i++) {
+        if (window.rhymes[stems[i]] == null) window.rhymes[stems[i]] = [];
+        window.rhymes[stems[i]].push(keys[i]);
+    }
+    
+    //Generate poem
     var sel = document.getElementById("poemType");
     var func = poemFunctions[sel.options[sel.selectedIndex].text];
     document.getElementById("poem").innerHTML = func().join("<br />");
